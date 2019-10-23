29c18d0f688573869e8c0b9a8483b7f60f707282
Jon Evans <joncodes+github@gmail.com>
2019-10-23 09:52:47 -0600

    Memoize publicify

--- app/models/image_upload.rb
+++ app/models/image_upload.rb
@@ -4,11 +4,13 @@ class ImageUpload
   attr_accessor :image
 
   def publicify
+    return @public_path if @public_path.present?
+
     file = File.open(image.path)
     new_file = File.open(Rails.root.join('public/uploads/', image.original_filename), 'w')
     new_file << file.read
     new_file.close
 
-    'uploads/' + image.original_filename
+    @public_path = 'uploads/' + image.original_filename
   end
 end
